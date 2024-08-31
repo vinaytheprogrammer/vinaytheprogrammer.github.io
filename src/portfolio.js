53f20a05a7670861503c8b39eb077a5605e528d3
vinaygupta.nitjsr <2022pgcsca008@nitjsr.ac.in>
2024-09-01 02:36:13 +0530

    portfolio updated

--- src/portfolio.js
+++ src/portfolio.js
@@ -1,19 +1,19 @@
 const header = {
   // all the properties are optional - can be left empty or deleted
   homepage: 'https://rjshkhr.github.io/cleanfolio',
-  title: 'JS.',
+  title: 'Meet Vinay',
 }
 
 const about = {
   // all the properties are optional - can be left empty or deleted
-  name: 'John Smith',
-  role: 'Front End Engineer',
+  name: 'Vinay Gupta',
+  role: 'Full Stack Developer',
   description:
-    'Adipisicing sit fugit ullam unde aliquid sequi Facilis soluta facilis perspiciatis corporis nulla aspernatur. Autem eligendi rerum delectus modi quisquam? Illo ut quasi nemo ipsa cumque perspiciatis! Maiores minima consectetur.',
-  resume: 'https://example.com',
+    'A Full Stack Developer designs and implements both front-end and back-end solutions, ensuring seamless integration and functionality across applications.',
+  resume: 'https://drive.google.com/file/d/1EitYjBipcmEfBKFUam_gHdJr7LLjr_6n/view?usp=sharing',
   social: {
-    linkedin: 'https://linkedin.com',
-    github: 'https://github.com',
+    linkedin: 'https://www.linkedin.com/in/vinaygupta-nitjsr/',
+    github: 'https://github.com/vinaytheprogrammer',
   },
 }
 
@@ -21,28 +21,52 @@ const projects = [
   // projects can be added an removed
   // if there are no projects, Projects section won't show up
   {
-    name: 'Project 1',
+    name: 'Dispensary',
     description:
-      'Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam',
-    stack: ['SASS', 'TypeScript', 'React'],
-    sourceCode: 'https://github.com',
-    livePreview: 'https://github.com',
+      'The Dispensary Management System streamlines inventory, tracks patient history, and validates patients, addressing campus dispensary needs effectively.',
+    stack: ['Reactjs', 'Tailwind', 'Express','Nodejs','MongoDB','Postman','passport'],
+    sourceCode: 'https://github.com/vinaytheprogrammer/Dispensary_management_sys',
+    livePreview: 'https://dispensary-management-sys.vercel.app/',
   },
   {
-    name: 'Project 2',
+    name: 'Blog_Post',
     description:
-      'Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam',
-    stack: ['SASS', 'TypeScript', 'React'],
-    sourceCode: 'https://github.com',
-    livePreview: 'https://github.com',
+      'This project provides user authentication, CRUD operations, commenting, profile management, and administrative controls, ensuring a complete and secure experience.',
+    stack: ['EJS', 'Mongoose', 'JWT','Thunderbolt'],
+    sourceCode: 'https://github.com/vinaytheprogrammer/Blog_Post',
+    livePreview: 'https://blog-post-75ip.onrender.com/',
   },
   {
-    name: 'Project 3',
+    name: 'ChitChat',
     description:
-      'Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam',
-    stack: ['SASS', 'TypeScript', 'React'],
-    sourceCode: 'https://github.com',
-    livePreview: 'https://github.com',
+      'This is an AI-powered chat application designed for engaging conversations, providing intelligent responses, and enhancing user interactions seamlessly.',
+    stack: ['JS', 'Gemini', 'CSS3'],
+    sourceCode: 'https://github.com/vinaytheprogrammer/Chitchat',
+    livePreview: 'https://chitchat-0tk9.onrender.com/',
+  },
+  {
+    name: 'LegisForum',
+    description:
+      'My first freelance project: a website designed for managing customer workload and providing a seamless interface for query resolution',
+    stack: ['Bootstrap', 'Slider', 'CSS'],
+    sourceCode: 'https://github.com/vinaytheprogrammer/legis_forum',
+    livePreview: 'https://legisforum.org/',
+  },
+  {
+    name: 'Weather_Report',
+    description:
+      'A WeatherApp utilizing an API to fetch and display real-time weather data, offering accurate forecasts and current conditions.',
+    stack: ['JS', 'Weather', 'ExpressJS'],
+    sourceCode: 'https://github.com/vinaytheprogrammer/weather_report',
+    livePreview: 'https://weather-report-mu-nine.vercel.app/',
+  },
+  {
+    name: 'Encyption Decryption',
+    description:
+      'Encryption and decryption in C++ using a custom algorithm that maps characters to emojis and handles file-based data storage.',
+    stack: ['CPP', 'FileHanding', 'STL'],
+    sourceCode: 'https://github.com/vinaytheprogrammer/Encryption_and_Decryption',
+    livePreview: '#',
   },
 ]
 
@@ -51,21 +75,21 @@ const skills = [
   // if there are no skills, Skills section won't show up
   'HTML',
   'CSS',
+  'Bootstrap',
   'JavaScript',
-  'TypeScript',
-  'React',
-  'Redux',
-  'SASS',
+  'ReactJS',
+  'EJS',
+  'MongoDB',
   'Material UI',
   'Git',
   'CI/CD',
-  'Jest',
-  'Enzyme',
+  'JWT',
+  'Passport',
 ]
 
 const contact = {
   // email is optional - if left empty Contact section won't show up
-  email: 'johnsmith@mail.com',
+  email: 'vinaygupta.nitjsr@gmail.com',
 }
 
 export { header, about, projects, skills, contact }
