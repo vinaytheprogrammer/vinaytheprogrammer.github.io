d9ecb766cae79bfe43420f4d8f9f9b5e8e8c32a2
vinaygupta.nitjsr <2022pgcsca008@nitjsr.ac.in>
2024-12-21 18:29:20 +0530

    portfolio data added

--- src/portfolio.js
+++ src/portfolio.js
@@ -1,77 +1,109 @@
 const header = {
-  homepage: 'https://vinaytheprogrammer.github.io/vinayGupta.io/',
-  title: 'Meet Vinay',
+  homepage: "https://vinaytheprogrammer.github.io/vinayGupta.io/",
+  title: "Meet Vinay",
 };
 
 const about = {
-  name: 'Vinay Gupta',
-  role: 'Full Stack Developer',
+  name: "Vinay Gupta",
+  role: "Full Stack Developer",
   description:
-    'A Full Stack Developer designs and implements both front-end and back-end solutions, ensuring seamless integration and functionality across applications.',
-  resume: 'https://drive.google.com/file/d/1EitYjBipcmEfBKFUam_gHdJr7LLjr_6n/view?usp=sharing',
+    "A Full Stack Developer designs and implements both front-end and back-end solutions, ensuring seamless integration and functionality across applications.",
+  resume:
+    "https://drive.google.com/file/d/1EitYjBipcmEfBKFUam_gHdJr7LLjr_6n/view?usp=sharing",
   social: {
-    linkedin: 'https://www.linkedin.com/in/vinaygupta-nitjsr/',
-    github: 'https://github.com/vinaytheprogrammer',
+    linkedin: "https://www.linkedin.com/in/vinaygupta-nitjsr/",
+    github: "https://github.com/vinaytheprogrammer",
   },
 };
 
 const projects = [
   {
-    name: 'Dispensary',
+    name: "Dispensary",
     description:
-      'The Dispensary Management System streamlines inventory, tracks patient history, and validates patients, addressing campus dispensary needs effectively.',
-    stack: ['Reactjs', 'Tailwind', 'Express', 'Nodejs', 'MongoDB', 'Postman', 'passport'],
-    sourceCode: 'https://github.com/vinaytheprogrammer/Dispensary_management_sys',
-    livePreview: 'https://dispensary-management-sys.vercel.app/',
+      "The Dispensary Management System streamlines inventory, tracks patient history, and validates patients, addressing campus dispensary needs effectively.",
+    stack: [
+      "Reactjs",
+      "Tailwind",
+      "Express",
+      "Nodejs",
+      "MongoDB",
+      "Postman",
+      "passport",
+    ],
+    sourceCode:
+      "https://github.com/vinaytheprogrammer/Dispensary_management_sys",
+    livePreview: "https://dispensary-management-sys.vercel.app/",
   },
   {
-    name: 'Blog_Post',
+    name: "Blog_Post",
     description:
-      'This project provides user authentication, CRUD operations, commenting, profile management, and administrative controls, ensuring a complete and secure experience.',
-    stack: ['EJS', 'Mongoose', 'JWT', 'Thunderbolt'],
-    sourceCode: 'https://github.com/vinaytheprogrammer/Blog_Post',
-    livePreview: 'https://blog-post-75ip.onrender.com/',
+      "This project provides user authentication, CRUD operations, commenting, profile management, and administrative controls, ensuring a complete and secure experience.",
+    stack: ["EJS", "Mongoose", "JWT", "Thunderbolt"],
+    sourceCode: "https://github.com/vinaytheprogrammer/Blog_Post",
+    livePreview: "https://blog-post-75ip.onrender.com/",
   },
   {
-    name: 'ChitChat',
+    name: "ChitChat",
     description:
-      'This is an AI-powered chat application designed for engaging conversations, providing intelligent responses, and enhancing user interactions seamlessly.',
-    stack: ['JS', 'Gemini', 'CSS3'],
-    sourceCode: 'https://github.com/vinaytheprogrammer/Chitchat',
-    livePreview: 'https://chitchat-0tk9.onrender.com/',
+      "This is an AI-powered chat application designed for engaging conversations, providing intelligent responses, and enhancing user interactions seamlessly.",
+    stack: ["JS", "Gemini", "CSS3"],
+    sourceCode: "https://github.com/vinaytheprogrammer/Chitchat",
+    livePreview: "https://chitchat-0tk9.onrender.com/",
   },
   {
-    name: 'LegisForum',
+    name: "LegisForum",
     description:
-      'My first freelance project: a website designed for managing customer workload and providing a seamless interface for query resolution',
-    stack: ['Bootstrap', 'Slider', 'CSS'],
-    sourceCode: 'https://github.com/vinaytheprogrammer/legis_forum',
-    livePreview: 'https://legisforum.org/',
+      "My first freelance project: a website designed for managing customer workload and providing a seamless interface for query resolution",
+    stack: ["Bootstrap", "Slider", "CSS"],
+    sourceCode: "https://github.com/vinaytheprogrammer/legis_forum",
+    livePreview: "https://legisforum.org/",
   },
   {
-    name: 'Weather_Report',
+    name: "Weather_Report",
     description:
-      'A WeatherApp utilizing an API to fetch and display real-time weather data, offering accurate forecasts and current conditions.',
-    stack: ['JS', 'Weather', 'ExpressJS'],
-    sourceCode: 'https://github.com/vinaytheprogrammer/weather_report',
-    livePreview: 'https://weather-report-mu-nine.vercel.app/',
+      "A WeatherApp utilizing an API to fetch and display real-time weather data, offering accurate forecasts and current conditions.",
+    stack: ["JS", "Weather", "ExpressJS"],
+    sourceCode: "https://github.com/vinaytheprogrammer/weather_report",
+    livePreview: "https://weather-report-mu-nine.vercel.app/",
   },
   {
-    name: 'Encryption Decryption',
+    name: "Encryption Decryption",
     description:
-      'Encryption and decryption in C++ using a custom algorithm that maps characters to emojis and handles file-based data storage.',
-    stack: ['CPP', 'FileHanding', 'STL'],
-    sourceCode: 'https://github.com/vinaytheprogrammer/Encryption_and_Decryption',
-    livePreview: '#',
+      "Encryption and decryption in C++ using a custom algorithm that maps characters to emojis and handles file-based data storage.",
+    stack: ["CPP", "FileHanding", "STL"],
+    sourceCode:
+      "https://github.com/vinaytheprogrammer/Encryption_and_Decryption",
+    livePreview: "#",
   },
 ];
 
 const skills = [
-  'HTML', 'CSS', 'Bootstrap', 'JavaScript', 'ReactJS', 'EJS', 'MongoDB', 'Material UI', 'Git', 'CI/CD', 'JWT', 'Passport',
+  "HTML",
+  "CSS",
+  "Bootstrap",
+  "JavaScript",
+  "ReactJS",
+  "EJS",
+  "MongoDB",
+  "Material UI",
+  "Git",
+  "CI/CD",
+  "JWT",
+  "Passport",
+  'Python',
+  'C',
+  'C++',
+  'SQL',
+  'Pandas',
+  'NumPy',
+  'Windows',
+  'Linux',
+  'Git',
+  'GitHub'
 ];
 
 const contact = {
-  email: 'vinaygupta.nitjsr@gmail.com',
+  email: "vinaygupta.nitjsr@gmail.com",
 };
 
 export { header, about, projects, skills, contact };
